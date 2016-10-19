1a1701b26d6341cce7f247adebaee481f4f123d5
new account <btang@london.da.cwi.nl>
2016-10-19 16:05:24 +0200

    readme

---
+++ config.h
@@ -0,0 +1,38 @@
+/* config.h.  Generated from config.h.in by configure.  */
+/* config.h.in.  Generated from configure.ac by autoheader.  */
+
+/* define if the Boost library is available */
+#define HAVE_BOOST /**/
+
+/* define if the Boost::Regex library is available */
+#define HAVE_BOOST_REGEX /**/
+
+/* define if the compiler supports basic C++11 syntax */
+#define HAVE_CXX11 1
+
+/* Define to 1 if you have the `sqlite3' library (-lsqlite3). */
+/* #undef HAVE_LIBSQLITE3 */
+
+/* Name of package */
+#define PACKAGE "sqlsmith"
+
+/* Define to the address where bug reports for this package should be sent. */
+#define PACKAGE_BUGREPORT "seltenreich@gmx.de"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "SQLsmith"
+
+/* Define to the full name and version of this package. */
+#define PACKAGE_STRING "SQLsmith 1.1"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "sqlsmith"
+
+/* Define to the home page for this package. */
+#define PACKAGE_URL "https://github.com/anse1/sqlsmith/"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.1"
+
+/* Version number of package */
+#define VERSION "1.1"
